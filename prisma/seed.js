086fc9c901ab90e9d643d0ea34f17d2111b57da1
Leonardo Lodi <your_email@address>
2024-06-28 14:40:00 +0100

    add profile model

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,27 +2,40 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function seed() {
-    const createdUsers = await prisma.user.createMany({
-        data: [
-            { 
-                username: 'alicemay', 
-                email: 'alicermay@gmail.com' 
-            },
-            { 
-                username: 'javiertech', 
-                email: 'javiertech@gmail.com' 
+    const usersPromise = [
+        {
+            username: 'alicemay', 
+            email: 'alicermay@gmail.com',
+            profile: {
+                create: {
+                    first_name: 'Alice',
+                    last_name: 'May',
+                    bio: 'I am Alice and i love coding :)',
+                    picture_url: 'date:image/alicemay.jpeg'
+                }
             }
-        ]
-    });
+        },
+        { 
+            username: 'javiertech', 
+            email: 'javiertech@gmail.com',
+            profile: {
+                create: {
+                    first_name: 'Javier',
+                    last_name: 'Xapiro',
+                    bio: 'Always on touch with new tech',
+                    picture_url: 'date:image/javierxapiro.jpeg'
+                }
+            }
+        }
+    ].map(user => prisma.user.create({
+        data: user,
+        include: {
+            profile: true
+        }
+    }))
 
-    console.log(`${createdUsers.count} users created`, createdUsers);
-
-    // Add your code here
-
-    
-
-
-    // Don't edit any of the code below this line
+    const users = await Promise.all(usersPromise)
+    console.log('users created', users);
     process.exit(0);
 }
 
